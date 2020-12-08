01197e45068ff587a31b5726ca2861dae2a82378
Lev <lmartens_43@hotmail.com>
2020-12-08 19:04:00 +1100

    before scaffold key test and _scrolcontroller.jumpTo(0) implementation.

---
+++ functions/src/index.ts
@@ -0,0 +1,46 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+const db = admin.firestore();
+
+import * as sgMail from '@sendgrid/mail';
+
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template();
+sgMail.setAPIKey(API_KEY);
+
+export const genericEmail = functions.https.onCall(async (data, context) => {
+
+if (!context.auth && !context.auth.token.mail) {
+throw new functions.https.HttpsError('failed-precondition', 'Must be logged in with an email address');
+}
+
+const msg = {
+    to: context.auth.token.email,
+    from: levmartens@gmail.com,
+    templateId: TEMPLATE_ID,
+    dynamic_template_data: {
+        subject: data.subject,
+        name: data.text,
+    }
+}
+
+await sgMail.send(msg);
+
+return { success: true };
+
+});
+
+
+
+
+
+
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
