74774e6f29b6b43e06e0a99068feb92074cecb9a
Lev <lmartens_43@hotmail.com>
2021-03-25 12:31:35 +1100

    removed comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -37,10 +37,3 @@ return { success: true };
 
 
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
